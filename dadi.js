b30e12fe5d67a994fac5af34ccc046ced9c86df1
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-06-20 13:42:02 +0200

    dadi.js

--- dadi.js
+++ dadi.js
@@ -5,3 +5,10 @@ console.log(userNumber)
 const computerNumber = Math.floor((Math.random() * 6)+1);
 console.log(computerNumber);
 
+if(userNumber > computerNumber){
+    console.log('hai vinto')
+} else if (userNumber < computerNumber){
+    console.log('hai perso')
+} else if(userNumber = computerNumber) {
+    console.log('pareggio, ritira')
+}
